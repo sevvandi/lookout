f116844ed1130ace2bd2819c9d5f16f79f1a52ee
Rob Hyndman <robjhyndman@gmail.com>
2021-01-12 11:16:03 +1100

    Changed name to persisting_outliers

--- R/outlier_persistence.R
+++ R/outlier_persistence.R
@@ -1,53 +1,64 @@
 #' Computes outlier persistence for a range of significance values.
 #'
-#' This function computes outlier persistence for a range of significance values, using the algorithm lookout, an outlier detection method that uses leave-one-out kernel density estimates and generalized Pareto distributions to find outliers.
+#' This function computes outlier persistence for a range of significance
+#' values, using the algorithm lookout, an outlier detection method that uses
+#' leave-one-out kernel density estimates and generalized Pareto distributions
+#' to find outliers.
 #'
-#' @param X The input data in a dataframe, matrix or tibble format.
-#' @param alpha_min The smallest level of significance, set to \code{0.01}.
-#' @param alpha_max The maximum level of significance, set to \code{0.1}.
-#' @param step The step size, set to \code{0.01}.
-#' @param st_qq The starting quantile for death radii sequence. This will be used to compute the starting bandwidth value.
-#' @param en_sc The scaling for the maximum death radii. This will be used to compute the biggest bandwidth value.
-#' @param unitize An option to normalize the data. Default is \code{TRUE}, which normalizes each column to \code{[0,1]}.
+#' @param X The input data in a matrix, data.frame, or tibble format. All
+#'   columns should be numeric.
+#' @param alpha Grid of significance levels.
+#' @param st_qq The starting quantile for death radii sequence. This will be
+#'   used to compute the starting bandwidth value.
+#' @param unitize An option to normalize the data. Default is \code{TRUE},
+#'   which normalizes each column to \code{[0,1]}.
 #' @param num_steps The length of the bandwidth sequence.
 #'
 #' @return A list with the following components:
-#' \item{\code{out}}{A 3D array of \code{N x num_steps x num_alpha} where \code{N} denotes the number of observations, num_steps denote the length of the bandwidth sequence and num_alpha denotes the number of significance levels. This is a binary array and the entries are set to 1 if that observation is an outlier for that particular bandwidth and significance level.}
+#' \item{\code{out}}{A 3D array of \code{N x num_steps x num_alpha} where
+#' \code{N} denotes the number of observations, num_steps denote the length
+#'   of the bandwidth sequence and num_alpha denotes the number of significance
+#'   levels. This is a binary array and the entries are set to 1 if that
+#'   observation is an outlier for that particular bandwidth and significance
+#'   level.}
 #' \item{\code{bw}}{The set of bandwidth values.}
 #' \item{\code{gpdparas}}{The GPD parameters used. }
-#' \item{\code{lookoutbw}}{The bandwidth chosen by the algorithm \code{lookout} using persistent homology.}
+#' \item{\code{lookoutbw}}{The bandwidth chosen by the algorithm \code{lookout}
+#'   using persistent homology.}
 #'
 #' @examples
-#' set.seed(1)
-#' x1 <- cbind.data.frame(rnorm(500), rnorm(500))
-#' x2 <- cbind.data.frame(rnorm(5, mean = 10, sd = 0.2), rnorm(5, mean = 10, sd = 0.2))
-#' colnames(x1) <- colnames(x2) <- c("x", "y")
-#' X <- rbind.data.frame(x1, x2)
+#' X <- rbind(
+#'   data.frame(x = rnorm(500),
+#'              y = rnorm(500)),
+#'   data.frame(x = rnorm(5, mean = 10, sd = 0.2),
+#'              y = rnorm(5, mean = 10, sd = 0.2))
+#' )
 #' plot(X, pch = 19)
-#' outnew <- persisting_outliers_over_alpha(X, unitize = FALSE)
+#' outliers <- persisting_outliers(X, unitize = FALSE)
+#' outliers
+#' autoplot(outliers)
 #' @export
 
-persisting_outliers_over_alpha <- function(X, alpha_min = 0.01, alpha_max = 0.1, step = 0.01, st_qq = 0.9,
-                                           en_sc = sqrt(5), unitize = TRUE, num_steps = 20) {
-  X <- as.data.frame(X)
-  num_cols <- NCOL(X)
-  num_alpha <- (alpha_max - alpha_min) / step + 1
-  alpha_seq <- seq(from = alpha_min, to = alpha_max, by = step)
+persisting_outliers <- function(X, alpha = seq(0.01, 0.1, by=0.01),
+                                st_qq = 0.9, unitize = TRUE, num_steps = 20) {
+  # Prepare X matrix
+  X <- as.matrix(X)
   if (unitize) {
     X <- unitize(X)
   }
-  if (num_cols == 1L) {
+
+  # Calculate persistent homology
+  if (NCOL(X) == 1L) {
     phom <- TDAstats::calculate_homology(dist(X), format = "distmat")
   } else {
     phom <- TDAstats::calculate_homology(X, dim = 0)
   }
 
+  # Find bandwiths
   death_radi <- phom[, 3L]
   qq_st <- quantile(death_radi, probs = st_qq)
-  qq_en <- max(death_radi) * en_sc
+  qq_en <- max(death_radi) * sqrt(5)
   bw_vals <- seq(qq_st, qq_en, length.out = num_steps)
-  output <- array(0, dim = c(dim(X)[1], num_steps, num_alpha))
-
   q_thres <- quantile(death_radi, probs = 0.5)
   dr_thres <- death_radi[death_radi >= q_thres]
   dr_thres_diff <- diff(dr_thres)
@@ -55,21 +66,27 @@ persisting_outliers_over_alpha <- function(X, alpha_min = 0.01, alpha_max = 0.1,
   ind1 <- min(which(death_radi >= q_thres))
   ind <- max_persist_ind + ind1 - 1L
   bw_fixed <- death_radi[ind] * sqrt(5)
-  lookoutobj1 <- lookoutliers(X, alpha = 0.05, unitize = FALSE, bw = bw_fixed)
-  paras <- lookoutobj1$gpd[1:2]
 
+  # Find outliers
+  lookoutobj1 <- lookout(X, alpha = 0.05, unitize = FALSE, bw = bw_fixed)
+  paras <- lookoutobj1$gpd[1:2]
+  output <- array(0, dim = c(dim(X)[1], num_steps, length(alpha)))
   for (i in seq_along(bw_vals)) {
-    lookoutobj <- lookoutliers_fixed_gpd(X, alpha = 0.05, unitize = FALSE, bw = bw_vals[i], gpd = paras)
-    for (j in seq(num_alpha)) {
-      outinds <- which(lookoutobj$outlier_probability < alpha_seq[j])
+    lookoutobj <- lookout(X, alpha = 0.05, unitize = FALSE,
+                               bw = bw_vals[i], gpd = paras)
+    for (j in seq_along(alpha)) {
+      outinds <- which(lookoutobj$outlier_probability < alpha[j])
       output[outinds, i, j] <- 1
     }
   }
 
-  list(
+  # Return results
+  structure(list(
     out = output,
     bw = bw_vals,
     gpdparas = paras,
-    lookoutbw = bw_fixed
-  )
+    lookoutbw = bw_fixed,
+    alpha = alpha,
+    call = match.call()
+  ), class="persistingoutliers")
 }
