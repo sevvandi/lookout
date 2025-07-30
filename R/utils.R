12010799fa5d58f3228f330fb769e754ac7ca32c
Rob J Hyndman <robjhyndman@gmail.com>
2025-07-30 15:29:54 +1000

    Removed code no longer being used

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,3 @@
-nsphere <- function(n = 3L, r = 1L, npts = 3145L) {
-  x <- matrix(1, nrow = npts, ncol = n)
-  for (i in seq(n - 1)) {
-    j <- n - i
-    theta <- seq(from = -pi, to = pi, length.out = npts)
-    theta1 <- sample(theta, size = length(theta))
-    x[, seq(j)] <- x[, seq(j)] * cos(theta1)
-    x[, (j + 1)] <- x[, (j + 1)] * sin(theta1)
-  }
-  return(x * r)
-}
-
 # Unitize each column of X
 unitize <- function(X) {
   for (col in seq_len(NCOL(X))) {
@@ -20,22 +8,4 @@ unitize <- function(X) {
     }
   }
   X
-}
-
-# Rob's function
-# Function to transform multivariate data X to have normal margins
-transform_normal <- function(X, transformation = c("YJ","BD")) {
-  transformation <- match.arg(transformation)
-  X <- as.matrix(X)
-  for (i in seq(NCOL(X))) {
-    if(transformation == "YJ") {
-      X[, i] <- bestNormalize::yeojohnson(X[, i],
-        lower = 0, upper = 1, standardize = FALSE
-      )$x.t
-    } else {
-      X[, i] <- bickeldoksum(X[, i])
-    }
-  }
-  X <- weird::mvscale(X, cov = NULL)
-  return(X[, ])
 }
